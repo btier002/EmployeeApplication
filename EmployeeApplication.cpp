aa1914a0593d873e3cb257ba085b5a9cc489b48a
Beatrix Tierney <Benedict.Tierney@outlook.com>
2024-11-18 16:35:07 -0500

    11/18 changes, Included log template file

--- EmployeeApplication.cpp
+++ EmployeeApplication.cpp
@@ -7,32 +7,71 @@
 #include <algorithm>
 #include <map>
 #include <random>
-#include <boost/uuid/uuid.h>
+#include <unordered_map>
+#include <string>
+#include <regex>
+#include <sstream>
 
 using namespace std;
 
+// Reads the employee log file
+std::unordered_map<std::string, std::string> readEmployeeLogs(const std::string& filename) {
+	std::unordered_map<std::string, std::string> employeeMap;
+	std::ifstream file(filename);
+	std::string line;
+
+	while (std::getline(file, line)) {
+		std::stringstream ss(line);
+		std::string name, address, phoneNumber, age, companyPosition, userID, hireDate, pay; // Reads the csv file containing these / 7 different attributes
+		// Do , ',', except for last getline
+		std::getline(ss, userID, ',');
+		std::getline(ss, name, ',');
+		// Etc...
+
+		// Then normalize phone number or other things...
+	}
+	return employeeMap;
+}
+
+
+
 int main() {
-	
-	//Starting a while true to then look for commands entered through the line.
+	std::unordered_map<std::string, std::string> employeeMap;
+	// Starting a while true to then look for commands entered through the line.
 	while (true) {
+		
 		string command;
 		cout << "Awaiting a start command (login,register,logout,)";
 		cin >> command;
+		// Read the Employee Log file
+		employeeMap = readEmployeeLogs("employeelog.txt");
 
+		// A stop condition for testing
+		if (command == "exit") {
+			break;
+		}
+		// Register
 		if (command == "register") {
 			string name;
 			string uniqueID;
 			fstream fout;
-			fout.open("EmployeeLogs.csv", ios::out | ios::app);
+			fout.open("EmployeeLogs.txt", ios::out | ios::app);
 
+			// Generates userID
+			
+
+
+			// Entering name
 			cout << "Enter your FullName: ";
 			cin >> name;
-			fout << name <<","
+			fout << name << ",";
 
 		} else if (command == "login") {
 			//User enters login code, if login code matches what is in the file then user is logged on, else it will give error.
 
 		}
 	}
+
+	return 0;
 }
 
